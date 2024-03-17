cda29d6a7db030e0893948e0e78fb717bfc9fb2b
anajalvarenga <alvarengaanajulia6@gmail.com>
2024-03-17 20:18:57 -0300

    feat: add useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,11 @@
+const useTheme = () => {
+    const theme = {
+        bgPrimary: '',
+        bgSecondary: '#FFFFFF',
+        radius: '4px',
+    }
+
+    return theme;
+}
+
+export default useTheme;
